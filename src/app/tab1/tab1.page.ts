06e6fa3fb27d330781f037f679535bb9aa22ba7a
15210036ojan <muhamadfauzanaqil@gmail.com>
2022-12-01 17:00:28 +0700

    first commit

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+ 
+// Import Add (before/after import please installation axios from cmd)
+import axios from 'axios';
+ 
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+ 
+  // Variabel GetData for array
+  public allData:any = [];
+ 
+ 
+  constructor() {
+ 
+    // Form Load GetData
+    this.GetData();
+  }
+ 
+ 
+  // Function GetData from API ---------------------------------------------------------------------------
+  async GetData(){
+    const res:any = await axios.get('http://localhost/api/get_data.php');
+ 
+    console.log(res.data);
+    this.allData = res.data.result;
+  }
+  // Function getData ---------------------------------------------------------------------------
+ 
+ 
+}
